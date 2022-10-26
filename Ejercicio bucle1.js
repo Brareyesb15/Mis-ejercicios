3f053cdcc8d64c417991b41d12f3c6849aa717b0
Brandon Reyes <brareyesb15@gmail.com>
2022-10-26 16:37:04 -0500

    Ejercicio bucle 1

---
+++ Ejercicio bucle1.js
@@ -0,0 +1,7 @@
+
+
+    function imprimiendoParesbienhecho (x)
+    {for (var i = 1; i <= x; i++) 
+    if (i % 2 === 0){console.log (i +"parsito")} 
+     else (i % 2 === 1); {console.log (i + "noparsito")}
+    }
