95084decbd3991e1cda54f16342420301ac83e9e
Brandon Reyes <brareyesb15@gmail.com>
2022-10-26 17:39:28 -0500

    ej bucle correcto

--- Ejercicio bucle1.js
+++ Ejercicio bucle1.js
@@ -1,7 +1,12 @@
 
 
-    function imprimiendoParesbienhecho (x)
-    {for (var i = 1; i <= x; i++) 
-    if (i % 2 === 0){console.log (i +"parsito")} 
-     else (i % 2 === 1); {console.log (i + "noparsito")}
-    }
+  function imprimiendoParesbienhecho (x)
+  {for (var i = 1; i <= x; i++) 
+  if (i % 2 === 0){console.log (i +"parsito")} 
+ else {console.log (i + "noparsito")}
+  }
+  
+
+
+
+
